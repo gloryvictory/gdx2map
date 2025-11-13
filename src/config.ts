4c7a06f6e87e2ee436f50bb3b94d42394d6f2edf
Glory <zamaraev@gmail.com>
2025-11-13 22:52:55 +0500

    worked with martin tile server last

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const TILE_SERVER_URL = 'http://r48-vgeohub01.zsniigg.local:3333';
 export const BACKEND_SERVER_URL = 'http://r48-vgeohub01.zsniigg.local:8001';
 export const RGF_URL = '/api/v1/report/rgf/';
-
+//
